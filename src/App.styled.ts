575f92ccc76ad6958736deba6d5c7ca2b5de917c
Aneta Kasprzak <anet.kasprzak@gmail.com>
2024-08-27 18:31:49 +0100

    initial styles

---
+++ src/App.styled.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div``;
+
+export const Form = styled.div`
+  height: 100px;
+  width: 100px;
+  background-color: #fff;
+`;
+
+export const Results = styled.div`
+  height: 100px;
+  width: 100px;
+  background-color: #133041;
+`;
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
-import "./App.css";
+import "./App.styled";
+import { Wrapper, Form, Results } from "./App.styled";
 
 function App() {
-  return <p>Hello</p>;
+  return (
+    <Wrapper>
+      <FormComponent />
+      <ResultsComponent />
+    </Wrapper>
+  );
+}
+
+function FormComponent() {
+  return <Form>form</Form>;
+}
+
+function ResultsComponent() {
+  return <Results>results</Results>;
 }
 
 export default App;
